d353a176da1c0099d161bbe03980f24c13f356ab
tamsi.besson@epitech.eu <tamsi.besson@epitech.eu>
2017-06-24 14:37:37 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={require('./back.jpg')} style={{width: 50, height: 50}}/>
+        <Text>I made the wr of the conn</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
